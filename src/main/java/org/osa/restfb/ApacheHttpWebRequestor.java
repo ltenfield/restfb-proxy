72e1024a1fac287cff2c3135c323c23a50fb3d67
Lawrence Turcotte <lawrence.turcotte@gmail.com>
2016-12-07 18:51:51 -0500

    completed other verbs PUT, DELETE and POST with attachments, no testing

--- src/main/java/org/osa/restfb/ApacheHttpWebRequestor.java
+++ src/main/java/org/osa/restfb/ApacheHttpWebRequestor.java
@@ -7,7 +7,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.Header;
 import org.apache.http.HeaderElement;
@@ -18,12 +22,17 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.InputStreamBody;
 import org.apache.http.util.EntityUtils;
 
 import com.restfb.BinaryAttachment;
 import com.restfb.DebugHeaderInfo;
+import com.restfb.Version;
 import com.restfb.WebRequestor;
+import com.restfb.util.StringUtils;
 
 public class ApacheHttpWebRequestor implements WebRequestor {
 
@@ -32,6 +41,8 @@ public class ApacheHttpWebRequestor implements WebRequestor {
 
 	HttpClient hc;
 	
+	DebugHeaderInfo debugHeaderInfo;
+
 	public ApacheHttpWebRequestor(HttpClient hc) {
 		this.hc = hc;
 	}
@@ -44,15 +55,23 @@ public class ApacheHttpWebRequestor implements WebRequestor {
 
 	@Override
 	public Response executePost(String url, String parameters) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		HttpPost httpPost = new HttpPost(url + parameters == null ? "" : parameters);
+		return execute(httpPost);
 	}
 
 	@Override
 	public Response executePost(String url, String parameters, BinaryAttachment... binaryAttachments)
 			throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		HttpPost httpPost = new HttpPost(url + parameters == null ? "" : parameters);
+		if (binaryAttachments.length > 0) {
+			MultipartEntityBuilder mpeb = MultipartEntityBuilder.create();
+			for (BinaryAttachment binaryAttachment : binaryAttachments) {
+				InputStreamBody isb = new InputStreamBody(binaryAttachment.getData(), binaryAttachment.getContentType());
+				mpeb.addPart(binaryAttachment.getFilename(), isb);
+			}
+			httpPost.setEntity(mpeb.build());
+		}
+		return execute(httpPost);
 	}
 
 	@Override
@@ -63,8 +82,29 @@ public class ApacheHttpWebRequestor implements WebRequestor {
 
 	@Override
 	public DebugHeaderInfo getDebugHeaderInfo() {
-		// TODO Auto-generated method stub
-		return null;
+		return debugHeaderInfo;
+	}
+
+	protected void fillHeaderAndDebugInfo(Map<String, Header> headerMap) {
+		//currentHeaders = Collections.unmodifiableMap(httpUrlConnection.getHeaderFields());
+
+		Header header = headerMap.get("facebook-api-version");
+		String usedApiVersion = StringUtils.trimToEmpty(header == null ? "" : header.getValue());
+		if (HTTP_LOGGER.isDebugEnabled()) {
+			HTTP_LOGGER.debug(format("Facebook used the API %s to answer your request", usedApiVersion));
+		}
+		header = headerMap.get("facebook-api-x-fb-trace-id");
+		String fbTraceId = StringUtils.trimToEmpty(header == null ? "" : header.getValue());
+		header = headerMap.get("x-fb-rev");
+		String fbRev = StringUtils.trimToEmpty(header == null ? "" : header.getValue());
+		header = headerMap.get("x-fb-debug");
+		String fbDebug = StringUtils.trimToEmpty(header == null ? "" : header.getValue());
+		header = headerMap.get("x-app-usage");
+		String fbAppUsage = StringUtils.trimToEmpty(header == null ? "" : header.getValue());
+		header = headerMap.get("x-page-usage");
+		String fbPageUsage = StringUtils.trimToEmpty(header == null ? "" : header.getValue());
+		Version usedVersion = Version.getVersionFromString(usedApiVersion);
+		debugHeaderInfo = new DebugHeaderInfo(fbDebug, fbRev, fbTraceId, usedVersion, fbAppUsage, fbPageUsage);
 	}
 
 	private Response execute(HttpRequestBase httpRequest) throws IOException, ClientProtocolException {
@@ -75,16 +115,20 @@ public class ApacheHttpWebRequestor implements WebRequestor {
 		HttpResponse httpResponse = hc.execute(httpRequest);
 		StatusLine statusLine = httpResponse.getStatusLine();
 		if (statusLine == null) throw new IllegalStateException("request statusline returned by HttpClient cannot be null");
+		Header[] headers = httpResponse.getAllHeaders();
 		HttpEntity entity = httpResponse.getEntity();
-		Header header = entity.getContentType();
-		HeaderElement[] elements = header.getElements();
 		if (entity == null) throw new IllegalStateException("request entity returned by HttpClient cannot be null");
-	      if (HTTP_LOGGER.isTraceEnabled()) {
-	          HTTP_LOGGER.trace(format("Response headers: %s", elements == null ? null : Arrays.toString(elements)));
-	        }
-
-		InputStream content = entity.getContent();
-		if (content == null) return null;		
+		if (HTTP_LOGGER.isTraceEnabled()) {
+		      HTTP_LOGGER.trace(format("Response headers: %s", headers == null ? null : Arrays.toString(headers)));
+		}
+		Map<String, Header> headerMap  = (headers == null || (headers.length < 1)) ? Collections.EMPTY_MAP : new HashMap<String, Header>(headers.length);
+		for (int i = 0;i < headers.length;i++) {
+			Header header = headers[i];
+			headerMap.put(header.getName(), header);
+		}
+		fillHeaderAndDebugInfo(headerMap);
+//		InputStream content = entity.getContent();
+//		if (content == null) return null;		
 //		StringBuilder sb = new StringBuilder();
 //		try (BufferedReader br = new BufferedReader(new InputStreamReader(content))) {
 //			for (int c;(c = br.read()) != -1;)	{
@@ -96,7 +140,7 @@ public class ApacheHttpWebRequestor implements WebRequestor {
 	      if (HTTP_LOGGER.isDebugEnabled()) {
 	          HTTP_LOGGER.debug(format("Facebook responded with %s", final_response));
 	        }
-
+	    httpRequest.releaseConnection();
 		return final_response;
 	}
 }
